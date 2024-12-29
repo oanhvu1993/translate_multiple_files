a254239aa326769c8529a3c55aaf1329b9af5bcc
oanhvu1993 <oanhvu93@gmail.com>
2024-12-29 10:06:34 +0700

    minor fix

--- translateFunction.js
+++ translateFunction.js
@@ -3,13 +3,13 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
 export const retrieveTranslation = async (text, language) => {
-
+    //Choose key and model
     const genAI = new GoogleGenerativeAI("AIzaSyDRDzwojrzz97W_UH8tq4q2EDR_uoTxRKg");
     const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
-
+    //Create request for AI
     const prompt = `Just show the result. Translate this to ${language} : ${text}`;
+    //Run request
     const translateText = await model.generateContent(prompt);
-    // console.log(translateText.response.text());
     return translateText.response.text();
 }
 
