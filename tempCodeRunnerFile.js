9d2b57222ac4cfb921ddfba5c56cae552511f719
oanhvu1993 <oanhvu93@gmail.com>
2025-01-01 14:50:58 +0700

    json update

--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-    // writeFile(`${newPath}/${item.replace("-en", `-${lang.short}`)}`, data, (err) => {
-    //     if (err) throw err;
-    //     console.log(`${item} has been translated`);
-    // });
