9dccd1f52f3038d7876b16ee28f098332c33d039
oanhvu1993 <oanhvu93@gmail.com>
2025-01-01 23:41:53 +0700

    Hi Vũ. Tks em nhiều vì đã làm application này. Có vẻ nó đang chạy khá tốt. Thầy đang chưa có nhiều thời gian để check kỹ phần chạy parallel. Tuy nhiên, thầy kiểm tra nhanh thì có vẻ nó đang work well. (bravo to you). Nếu có time thì em có thể improve lên 1 tí xíu nữa thì nó sẽ perfect cho application này. Thầy có quay 1 clip để nói rõ nó cũng easy à (some config source) http://www.fsend.vn/download/2r_aeLOvm2sI3SuZF8s2dND-OU8x5O_e.
    Cám ơn em nhiều nhé.

--- app_v1.2.js
+++ app_v1.2.js
@@ -1,36 +1,31 @@
 import fs, { writeFile } from "fs";
-import { mkdir } from 'fs';
 import { retrieveTranslation } from "./translateFunction.js";
+import { configDotenv } from "dotenv";
 
-
-const folderPath = 'Archive/';
+configDotenv();
+const folderPath = process.env.FOLDER_PATH;
 const language = {
-    long: "Vietnamese",
-    short: "vi"
-};
+    name: process.env.LANGUAGE_NAME,
+    code: process.env.LANGUAGE_CODE
+}
 
 async function translateAllSrtFilesInFolder(path, lang) {
-    const filesInFolder = fs.readdirSync(path);
-    const newPath = path + lang.long;
-    //////////////////Create Folder Contain Translate Files//////////////////
-    mkdir(newPath, { recursive: true }, (err) => {
-        if (err) throw err;
-    });
-
+    const filesInFolder = fs.readdirSync(path, { recursive: true });
 
     //////////////////Get only SRT files//////////////////
     const srtFiles = filesInFolder.filter(function (file) {
         return file.endsWith('.srt');
     });
 
+    // console.log(srtFiles)
 
     ////////////////Commit action translate//////////////////
     for (const item of srtFiles) {
         // retrieve content in srt file
-        const content = fs.readFileSync(`${path}${item}`, 'utf-8');
+        const content = fs.readFileSync(`${path}/${item}`, 'utf-8');
         // transalte and put in new folder
-        retrieveTranslation(content, lang.long).then((data) => {
-            writeFile(`${newPath}/${item.replace("en.srt", `${lang.short}.srt`)}`, data, (err) => {
+        retrieveTranslation(content, lang.name).then((data) => {
+            writeFile(`${path}/${item.replace("en.srt", `${lang.code}.srt`)}`, data, (err) => {
                 if (err) throw err;
                 console.log(`${item} has been translated`);
             });
